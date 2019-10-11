5f46ba329012eeedf2a0b55b4c8cca0b46485a30
Onwuzulike Emeka <onwuzulikee1@gmail.com>
2019-10-12 00:37:41 +0100

    added db initialization

--- IShopify.IntegrationTests/ProductRepositoryTests.cs
+++ IShopify.IntegrationTests/ProductRepositoryTests.cs
@@ -19,6 +19,8 @@ namespace IShopify.IntegrationTests
         public ProductRepositoryTests()
         {
             _container = IocConfig.Register();
+            var db = _container.Resolve<DatabaseInitializer>();
+            db.initialize();
         }
 
         [Fact]
