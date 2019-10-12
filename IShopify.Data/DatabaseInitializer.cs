a6b09ea96b4c9c694a385c6f6bf60f06a33d94d1
texyh <onwuzulikee1@gmail.com>
2019-10-12 16:49:16 +0100

    Revert "Crud produccts (#11)"
    This reverts commit 9ae54e3f80346679a0175c3d2e572115e996eed4.

--- IShopify.Data/DatabaseInitializer.cs
+++ IShopify.Data/DatabaseInitializer.cs
@@ -1,5 +1,4 @@
 ﻿using IShopify.Core.Common;
-using IShopify.Core.Customer.Models;
 using IShopify.Core.Helpers;
 using IShopify.Core.Products.Models;
 using Microsoft.EntityFrameworkCore;
@@ -47,24 +46,6 @@ namespace IShopify.Data
             await AddProductAsync();
             await AddDeparmentAsync();
             await AddCategoryAsync();
-            await AddCustomersAsync();
-        }
-    
-        private async Task AddCustomersAsync() 
-        {
-            if(!await _dbContext.Customers.AnyAsync()) 
-            {
-                var customer = new CustomerEntity 
-                {
-                    Name = "Emeka",
-                    Email = "emeka@example.com",
-                    Password = "86e814aaba9f81d764c63e2e02233b55234dacc862637bafd6c549928770c448"
-                };
-
-                 _dbContext.Customers.Add(customer);
-
-                await _dbContext.SaveChangesAsync();
-            }
         }
 
         private async Task AddProductAsync()
@@ -75,7 +56,7 @@ namespace IShopify.Data
                 var products = productString.FromJson<IList<ProductEntity>>();
                 products.ForEach(x => x.Id = 0);
 
-                _dbContext.Products.AddRange(products);
+                await _dbContext.Products.AddRangeAsync(products);
 
                 await _dbContext.SaveChangesAsync();
             }
@@ -89,7 +70,7 @@ namespace IShopify.Data
                 var categories = categoryString.FromJson<IList<CategoryEntity>>();
                 categories.ForEach(x => x.Id =0);
 
-                 _dbContext.Categories.AddRange(categories);
+                await _dbContext.Categories.AddRangeAsync(categories);
 
                 await _dbContext.SaveChangesAsync();
             }
@@ -103,7 +84,7 @@ namespace IShopify.Data
                 var departments = departmentString.FromJson<IList<DepartmentEntity>>();
                 departments.ForEach(x => x.Id = 0);
 
-                _dbContext.Departments.AddRange(departments);
+                await _dbContext.Departments.AddRangeAsync(departments);
 
                 await _dbContext.SaveChangesAsync();
             }
