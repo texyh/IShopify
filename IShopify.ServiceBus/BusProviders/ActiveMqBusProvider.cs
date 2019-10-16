9726b0112a12ce0f248e972fef066f1e5d94df44
Onwuzulike Emeka <onwuzulikee1@gmail.com>
2019-10-16 23:25:07 +0100

    added activemq provider

---
+++ IShopify.ServiceBus/BusProviders/ActiveMqBusProvider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using IShopify.Core.MessageBus;
+using MassTransit;
+
+namespace IShopify.ServiceBus.BusProviders
+{
+    public class ActiveMqBusProvider : IBusProvider
+    {
+        public IBusControl CreateBus(params MessageRoute[] routes)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
